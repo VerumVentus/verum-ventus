614f9b03203d53b07908b2ae1a85a05c901ca374
Jarred Luzada <jarredluzada@gmail.com>
2023-05-17 18:22:39 +0800

    Test for dynamic routing.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Pages({
+      onRoutesGenerated(routes) {
+        const transformRoute = (route) => ({
+          ...route,
+          path: route.path.replace('/:@', '/@:'),
+          children: route.children?.map(transformRoute) ?? [],
+        });
+
+        return routes.map(transformRoute);
+      },
+    }),
+  ],
   base: '/',
   resolve: {
     alias: {
